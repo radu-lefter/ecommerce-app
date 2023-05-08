f40e3aab2421f5f778a7f1391ed48f4ae29b83e5
Radu Lefter <lefter_radu@yahoo.com>
2023-05-08 18:11:45 +0100

    created dummy data and displayed it on front page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import { sampleProducts } from './data'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <header>TS Amazona</header>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
+      <footer>All rights reserved</footer>
+    </div>
+  )
+}
+
+export default App
