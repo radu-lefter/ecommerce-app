4c01b149d3ed14388e205a023854d07dda4d654f
Radu Lefter <lefter_radu@yahoo.com>
2023-05-09 22:06:44 +0100

    refactored backend to connect to db through model and routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,25 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import { sampleProducts } from './data'
 import cors from 'cors'
+import { productRouter } from './routers/productRouter'
+import { seedRouter } from './routers/seedRouter'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
+
+//process.env.MONGODB_URI || 
+
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/ecommercedb'
+mongoose.set('strictQuery', true)
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to mongodb')
+  })
+  .catch(() => {
+    console.log('error mongodb')
+  })
 
 const app = express()
 app.use(
@@ -9,13 +28,9 @@ app.use(
     origin: ['http://localhost:5173'],
   })
 )
-app.get('/api/products', (req: Request, res: Response) => {
-  res.json(sampleProducts)
-})
 
-app.get('/api/products/:slug', (req: Request, res: Response) => {
-  res.json(sampleProducts.find((x) => x.slug === req.params.slug))
-})
+app.use('/api/products', productRouter)
+app.use('/api/seed', seedRouter)
 
 const PORT = 4000
 app.listen(PORT, () => {
