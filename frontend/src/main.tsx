ced75bea7f726519ba8518ddfc7bb39252dc9d82
Radu Lefter <lefter_radu@yahoo.com>
2023-05-09 15:30:57 +0100

    implemented dark mode with context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,12 +8,12 @@ import {
 } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import { HelmetProvider } from 'react-helmet-async';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { StoreProvider } from './Store';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -30,11 +30,13 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </HelmetProvider>
+    <StoreProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </HelmetProvider>
+    </StoreProvider>
   </React.StrictMode>
 );
