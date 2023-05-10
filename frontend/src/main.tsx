b992158ab96c70a2c86510a0baf59d6c2e921359
Radu Lefter <lefter_radu@yahoo.com>
2023-05-10 16:14:25 +0100

    created shipping adress page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,6 +17,7 @@ import { StoreProvider } from './Store';
 import CartPage from './pages/CartPage'
 import SigninPage from './pages/SigninPage'
 import SignupPage from './pages/SignupPage'
+import ShippingAddressPage from './pages/ShippingAddressPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,6 +27,7 @@ const router = createBrowserRouter(
       <Route path="cart" element={<CartPage />} />
       <Route path="signin" element={<SigninPage />} />
       <Route path="signup" element={<SignupPage />} />
+      <Route path="shipping" element={<ShippingAddressPage />} />
       {/* <Route path="dashboard" element={<Dashboard />} /> */}
       {/* ... etc. */}
     </Route>
