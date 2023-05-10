01bac718c11e4635436d6139eb6b3c74d0ca324b
Radu Lefter <lefter_radu@yahoo.com>
2023-05-10 15:40:02 +0100

    created signin page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -15,6 +15,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StoreProvider } from './Store';
 import CartPage from './pages/CartPage'
+import SigninPage from './pages/SigninPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,6 +23,7 @@ const router = createBrowserRouter(
       <Route index={true} element={<HomePage />} />
       <Route path="product/:slug" element={<ProductPage />} />
       <Route path="cart" element={<CartPage />} />
+      <Route path="signin" element={<SigninPage />} />
       {/* <Route path="dashboard" element={<Dashboard />} /> */}
       {/* ... etc. */}
     </Route>
