b880533f55b1f3d8401e35bd8e285b4083130333
Radu Lefter <lefter_radu@yahoo.com>
2023-05-10 16:25:09 +0100

    created backend place order functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import { seedRouter } from './routers/seedRouter'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import { userRouter } from './routers/userRouter'
+import { orderRouter } from './routers/orderRouter'
 
 dotenv.config()
 
@@ -35,6 +36,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/products', productRouter)
 app.use('/api/users', userRouter)
+app.use('/api/orders', orderRouter)
 app.use('/api/seed', seedRouter)
 
 const PORT = 4000
